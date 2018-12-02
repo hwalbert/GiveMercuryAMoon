c5d0cbc1247be16d4c01c33ac96a21edb1c575e8
hwalbert <hwalbert@users.noreply.github.com>
2018-12-02 16:10:57 -0500

    Broke out into own file

--- meRcury/R/ExtractEgypt.R
+++ meRcury/R/ExtractEgypt.R
@@ -1,49 +1,25 @@
-#
-#
-ExtractEGYPT <- function(GDELTData, writeResults = T){
+# rewrite some functions
+ExtractEGYPT <- function(GDELTData, location = '~/GitHub/GiveMercuryAMoon/Data/'){
+  #Location filter
+  vec.EgyptLocation <-
+    c("egypt")
   #Look for Organizations:####
-  cat("Finding Egypt Specific Organizations... \n")
-  EgyptOrganizations <- c("muslim brotherhood", "egyptian police", "egyptian military", "egyptian army", "egyptian", "Harakat Sawa'd Misr", "Harakat Sawad Misr", "HASM", "ISIS-Sinai", "ISIS Sinai", "Liwa al-Thawra")
-  AllOrganizations <- data.frame()
-  for (i in EgyptOrganizations){
-    Result <- filter(GDELTData, grepl(tolower(i), tolower(organizations)))
-    AllOrganizations <- rbind(Result, AllOrganizations)
-  }
-  AllOrganizations <- unique(AllOrganizations)
-
+  vec.EgyptOrganizations <-
+    c("muslim brotherhood", "egyptian", "Harakat Sawa'd Misr", "Harakat Sawad Misr",
+      "HASM", "ISIS-Sinai", "ISIS Sinai", "Liwa al-Thawra","sisi")
   #Look for Themes:####
-  #Grab the GDELT Codebook
-  #GDELT_Codebook <- get_gdelt_codebook_ft_api(code_book = "gkg")
-  #Look at some of the Egypt themes in the GDELT Codebook:
-  cat("Finding Egypt Specific Themes... \n")
-  EgyptThemes <- c(grep("EGYPT", GDELT_Codebook$idGKGTheme, value = T))
-
-  AllThemes <- data.frame()
-  for (i in EgyptThemes){
-    Result <- filter(GDELTData, grepl(tolower(i), tolower(themes)))
-    AllThemes <- rbind(Result, AllThemes)
-  }
-  AllThemes <- unique(AllThemes)
-
-  cat("Finding Egypt Specific Domain Names... \n")
-  AllDomains <- data.frame()
-  EgyptDomains <- c("almasryalyoum.com", "ahram.org.eg", "alwafd.org", "youm7.com", "egyptindependent.com", "dailynewsegypt.com")
-  for (i in EgyptDomains){
-    Result <- filter(GDELTData, grepl(tolower(i), tolower(domainSource)))
-    AllDomains <- rbind(Result, AllDomains)
-  }
-  AllDomains <- unique(AllDomains)
-
-
-  AllData <- rbind(AllOrganizations, AllThemes, AllDomains)
-  AllData <- unique(AllData)
-
-
-
-  EgyptData <<- AllData
-
-  if(writeResults==T){
-    FolderForResults <- choose.dir(caption = "Select where to store Egypt specific GDELT Results")
-    fwrite(EgyptData, file = paste(FolderForResults, "/EgyptDataDate", dates, ".csv", sep = ""))
-  }
+  vec.EgyptThemes <- c('revolution','strike','econ_inflation','grievance','arrest','rebellion',
+                       'corruption','conflict','violence','protest','terror','unrest')
+  #Look for domains:
+  vec.EgyptDomains <-
+    c("almasryalyoum.com", "ahram.org.eg", "alwafd.org", "youm7.com",
+      "egyptindependent.com", "dailynewsegypt.com","alrai.com",
+      'wsj.com','al-monitor.com')
+  filtered.gdelt<-
+    (data.table(GDELTData)
+     [grepl(paste0(vec.EgyptLocation,collapse = "|"), tolower(locations))]
+     [grepl(paste0(vec.EgyptOrganizations,collapse = "|"), tolower(organizations))]
+     [grepl(paste0(vec.EgyptThemes,collapse = "|"), tolower(themes))]
+     [grepl(paste0(vec.EgyptDomains,collapse = "|"), tolower(domainSource))])
+  return(filtered.gdelt)
 }
