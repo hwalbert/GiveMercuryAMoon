bdb32f65596d8ca19e340867946ce986f310c35c
hwalbert <hwalbert@users.noreply.github.com>
2018-12-02 16:10:41 -0500

    Updated code
    updated location specification code for use in model forecast workflow

--- meRcury/R/GetGDELT.R
+++ meRcury/R/GetGDELT.R
@@ -1,6 +1,18 @@
+#' @param DaysBack
+#'
+#' @param writeResults
+#' @param SpecificDates
+#' @param location
+#'
+#' @title ETL Egypt GDELT data
+#' @description # a giant for loop that downloads gdelt data for a given month, extracts the egypt related info, combines it, saves it to a master folder, and deletes them if all of the above is done successfully
+#'
+#' @return Returns a data frame to the global environment showing the debit/credit results.
+#' @examples DebitCreditCompare(MasterFile)
 
-GetGDELT <- function(DaysBack = 1, writeResults = T, SpecificDates = NULL){
 
+GetGDELT <- function(DaysBack = 1, writeResults = T, SpecificDates = NULL, location = "~/GitHub/GiveMercuryAMoon/Data", Engine = "Egypt"){
+cat("Preparing the Full Global Knowledge Graph \n")
   if(is.null(SpecificDates)){
     dates = DatesForGDELT(daysBack = DaysBack)
   }else{
@@ -8,24 +20,28 @@ GetGDELT <- function(DaysBack = 1, writeResults = T, SpecificDates = NULL){
   }
 
 
+
   #This code grabs the Global Knowledge Graph data for the dates specified by the DatesForGDELT() function
   GDELTData <-
-    get_data_gkg_days_detailed(
+    suppressMessages(get_data_gkg_days_detailed(
       dates = dates,
-      #dates = c("2018-09-08"),
+      # dates = c("2018-09-08"),
       table_name = 'gkg',
       return_message = T
-    )
+    ))
 
-  dates <<- dates
-  GDELTData <<- GDELTData
+  if(nrow(GDELTData) < 1){stop("The pulled GDELT Data has no rows.")}
 
-  #dates <- gsub("-", "_", dates)
-  if(writeResults==T){
-    FolderForResults <- choose.dir(caption = "Select where to store GDELT Results")
-    fwrite(GDELTData, file = paste(FolderForResults, "/GDELTDataDate", dates, ".csv", sep = ""))
+  if(Engine == "Egypt"){
+    GDELTData<-ExtractEGYPT(GDELTData)
+    #dates <- gsub("-", "_", dates)
+    if(writeResults==T){
+      FolderForResults <- location
+      fwrite(GDELTData, file = paste(FolderForResults, "/GDELTDataDate", dates, ".csv", sep = ""))
+    }
   }
 
+
+
+
 }
-
-
