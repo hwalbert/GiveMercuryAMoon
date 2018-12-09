4579ad7b479b9d275e6e883ab72332ed328b8275
zk-guidehouse <42655333+zk-guidehouse@users.noreply.github.com>
2018-12-09 17:09:42 -0500

    One more edit to GSR pulls
    Added special function for MA and remove the original "all GSR" pull

--- Code/update.MA.GSR.R
+++ Code/update.MA.GSR.R
@@ -9,12 +9,12 @@ library(jsonlite)
 library(data.table)
 library(curl)
 
-update.CU.GSR <- function() {
-  file <- "~\\GitHub\\GiveMercuryAMoon\\Data\\GSR_Civil_Unrest.csv"
-  cu.data <- fread(file.CU)
-  n.old <- nrow(cu.data)
+update.MA.GSR <- function() {
+  file.MA <- "~\\GitHub\\GiveMercuryAMoon\\Data\\GSR_Military_Activity.csv"
+  ma.data <- fread(file.MA)
+  n.old <- nrow(ma.data)
   
-  latest <- max( as.Date( x = cu.data$Event_Date, format = "%m/%d/%Y" ) )
+  latest <- max( as.Date( x = ma.data$Event_Date, format = "%Y-%m-%d" ) )
   today <- Sys.Date()
   
   diff.year <- year(today) - year(latest)
@@ -29,14 +29,14 @@ update.CU.GSR <- function() {
       i.month <- month.name[12]
     }
     i.year <- year(latest) + floor( ( month(latest) + i ) / 12.001)
-    i.url <- paste0("https://raw.githubusercontent.com/planetmercury/mercury-challenge/master/data/gsr/cu_gsr/CU_", 
+    i.url <- paste0("https://raw.githubusercontent.com/planetmercury/mercury-challenge/master/data/gsr/ma_gsr/MA_", 
                     i.month, "_", i.year, ".json")
     # Grab new data & check for any returned
     new.data <- fromJSON(i.url)
     
-    output <- unique.data.frame( rbind(cu.data, new.data) ) 
+    output <- unique.data.frame( rbind(ma.data, new.data) ) 
     n.rows.added <- nrow(output) - n.old
-    print(paste0("There were ", n.rows.added, " records to the GSR Civil Unrest dataset.") )
+    print(paste0("There were ", n.rows.added, " records to the GSR Military Activity dataset.") )
     return(output)  
   }
 }
