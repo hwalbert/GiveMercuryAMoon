bcd2e3adb9b34b42a9805393911c6341510d99a5
eugenechoGH <44753441+eugenechoGH@users.noreply.github.com>
2018-12-09 18:04:20 -0500

    harold's changes

--- Code/update.MA.GSR.R
+++ Code/update.MA.GSR.R
@@ -5,22 +5,20 @@
 #'
 #' @examples
 
-library(jsonlite)
-library(data.table)
-library(curl)
 
-update.MA.GSR <- function() {
+update.MA.GSR <- function(writeResults = F) {
   file.MA <- "~\\GitHub\\GiveMercuryAMoon\\Data\\GSR_Military_Activity.csv"
   ma.data <- fread(file.MA)
   n.old <- nrow(ma.data)
-  
+
   latest <- max( as.Date( x = ma.data$Event_Date, format = "%Y-%m-%d" ) )
   today <- Sys.Date()
-  
+
   diff.year <- year(today) - year(latest)
   diff.month <- month(today) - month(latest)
   n.months <- (12*diff.year) + diff.month
-  
+  #cat(as.Date(latest, origin = "1970-01-01"), "\n")
+
   # Pull in GSR data from Github
   for ( i  in 0:n.months ) {
     # Set URL for the given month & year
@@ -29,14 +27,16 @@ update.MA.GSR <- function() {
       i.month <- month.name[12]
     }
     i.year <- year(latest) + floor( ( month(latest) + i ) / 12.001)
-    i.url <- paste0("https://raw.githubusercontent.com/planetmercury/mercury-challenge/master/data/gsr/ma_gsr/MA_", 
+    i.url <- paste0("https://raw.githubusercontent.com/planetmercury/mercury-challenge/master/data/gsr/ma_gsr/MA_",
                     i.month, "_", i.year, ".json")
     # Grab new data & check for any returned
     new.data <- fromJSON(i.url)
-    
-    output <- unique.data.frame( rbind(ma.data, new.data) ) 
+
+    output <- unique.data.frame( rbind(ma.data, new.data) )
     n.rows.added <- nrow(output) - n.old
     print(paste0("There were ", n.rows.added, " records to the GSR Military Activity dataset.") )
-    return(output)  
+    if(writeResults == T){write.csv(file = file.MA, output, row.names = F)}
+
+    return(output)
   }
 }
---
+++ meRcury/.Rproj.user/AF408395/sources/s-8626E663/68C8FCC1-contents
@@ -0,0 +1,41 @@
+
+
+CreateEgyptPrediction <- function(){
+
+GDELT <- Aggregate_GDELT()
+
+tones <- cbind(GDELT$Date, as.data.frame(parse_gkg_mentioned_article_tone(gdelt_data = GDELT)) )
+names(tones) <- c("Date", "idGKG", "Tone", "Pos.Tone", "Neg.Tone", "Polarity", "Active.Refs", "Self.Ref", "Word.Count")
+attach(tones)
+agg <- aggregate(x = tones, by = list(Date), FUN = mean)[,-c(1,3)]
+detach(tones)
+negTone <- mean(agg$Neg.Tone[(length(agg$Neg.Tone)-6):length(agg$Neg.Tone)], na.rm = T)
+
+
+ModelInput <- data.frame(
+  temp = get_weather()[3,2],
+  is_weekend = ifelse(weekdays(Sys.Date() + 3) %in% c("Friday", "Saturday"),1,0),
+  avgnegtone = negTone)
+
+PREDICTION <- round(predict.lm(linreg1, ModelInput))
+Warning_ID <- paste0("GMAM-CU-", as.numeric(Sys.Date()))
+
+# Create the submission as a data.frame
+my.submission <- data.frame(
+  "Warning_ID" = Warning_ID,
+  "Event_Type" = "Civil Unrest Daily",
+  "Event_Date" = Sys.Date()+3,
+  "Country" = "Egypt",
+  "Count" = PREDICTION
+)
+
+# Write the submission to a .json file
+write_json(x = my.submission,
+           path = paste0("~/GitHub/GiveMercuryAMoon/Data/",Warning_ID, ".json")
+)
+
+
+
+return(round(PREDICTION))
+
+}
